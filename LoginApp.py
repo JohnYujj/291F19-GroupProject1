dc1e384a85bd294c87890c588550cfea54d7e2c7
JohnYujj <35233051+JohnYujj@users.noreply.github.com>
2019-10-20 08:27:19 -0600

    separated sqlcontrol class from loginapp

--- LoginApp.py
+++ LoginApp.py
@@ -1,22 +1,7 @@
 import tkinter
 from tkinter import *
 import sqlite3
-
-
-class SQLController:
-    def __init__(self, database):
-        self.connection = sqlite3.connect(database)
-        self.cursor = self.connection.cursor()
-        
-    #def check_if_userExists returns bool depending if select from users uid=uid found or not
-    #def check_if_uid-pw_match returns bool depending if select user uid/pwd match found or not
-    
-    def GetUserType(self, username,password):
-        self.cursor.execute('SELECT utype FROM users WHERE uid=:username AND pwd=:password',{"username":username,"password":password})
-        user = self.cursor.fetchone()
-        uType=user[0]
-        self.connection.commit()
-        return uType        
+import SQLControlClass
 
 class LoginApp(tkinter.Tk):
     def __init__(self, SQL):
@@ -65,7 +50,7 @@ class LoginApp(tkinter.Tk):
 
 def main():
     database = './test.db'
-    appSQL = SQLController(database)
+    appSQL = SQLControlClass.SQLController(database)
     
     winLogin = LoginApp(appSQL)
     winLogin.mainloop()
---
+++ SQLControlClass.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+class SQLController:
+    def __init__(self, database):
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+        
+    #def check_if_userExists returns bool depending if select from users uid=uid found or not
+    #def check_if_uid-pw_match returns bool depending if select user uid/pwd match found or not
+    
+    def GetUserType(self, username,password):
+        self.cursor.execute('SELECT utype FROM users WHERE uid=:username AND pwd=:password',{"username":username,"password":password})
+        user = self.cursor.fetchone()
+        uType=user[0]
+        self.connection.commit()
+        return uType     
