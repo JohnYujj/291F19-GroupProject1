804b9b0bfc3b73a553d490235b087b7d30eacd38
9614 <46434108+9614@users.noreply.github.com>
2019-11-04 00:22:44 -0700

    Update SQLControlClass.py

--- SQLControlClass.py
+++ SQLControlClass.py
@@ -172,20 +172,20 @@ class SQLController:
     
 
     def AbstractLife(self,fname,lname):
-        self.cursor.execute('select count(ddate), sum(points) from demeritNotices where fname = :fname and lname = :lname',{'fname':fname,'lname':lname})
+        self.cursor.execute('select count(ddate), sum(points) from demeritNotices where fname = :fname collate nocase and lname = :lname collate nocase',{'fname':fname,'lname':lname})
         return self.cursor.fetchall()
     
     def AbstractYear(self,fname,lname):
-        self.cursor.execute('select count(ddate), sum(points) from demeritNotices where fname = :fname and lname = :lname and ddate > date("now", "-2 years")',{'fname':fname,'lname':lname})
+        self.cursor.execute('select count(ddate), sum(points) from demeritNotices where fname = :fname collate nocase and lname = :lname collate nocase and ddate > date("now", "-2 years")',{'fname':fname,'lname':lname})
         return self.cursor.fetchall()
         
     def TAbstract(self,fname,lname):
-        self.cursor.execute('select count(t1.tno),count(t2.tno) from tickets t1, tickets t2 where t1.regno in (select regno from registrations where fname = :fname and lname = :lname) and t2.regno in (select regno from registrations where fname = :fname and lname = :lname) and t2.vdate > date("now", "-2 years")',{'fname':fname,'lname':lname})
+        self.cursor.execute('select count(t1.tno),count(t2.tno) from tickets t1, tickets t2 where t1.regno in (select regno from registrations where fname = :fname collate nocase and lname = :lname collate nocase) and t2.regno in (select regno from registrations where fname = :fname collate nocase and lname = :lname collate nocase) and t2.vdate > date("now", "-2 years")',{'fname':fname,'lname':lname})
         return self.cursor.fetchall()
 
     
     def TicketView(self,fname,lname):
-        self.cursor.execute('select tno, vdate, violation, fine, t.regno, make, model from tickets t, registrations r, vehicles v where r.regno = t.regno and r.vin = v.vin and r.fname = :fname and r.lname = :lname order by vdate desc',{"fname":fname,"lname":lname})
+        self.cursor.execute('select tno, vdate, violation, fine, t.regno, make, model from tickets t, registrations r, vehicles v where r.regno = t.regno and r.vin = v.vin and r.fname = :fname collate nocase and r.lname = :lname collate nocase order by vdate desc',{"fname":fname,"lname":lname})
         return self.cursor.fetchall()
     
     def MarriageReg(self,regno,regdate,regplace,fname1,lname1,fname2,lname2):
@@ -193,7 +193,7 @@ class SQLController:
         self.connection.commit()
     
     def CheckUniqueMarriageRegno(self,regno):
-        self.cursor.execute('SELECT * FROM marriages WHERE regno=:regno',{"regno":regno})
+        self.cursor.execute('SELECT * FROM marriages WHERE regno=:regno collate nocase',{"regno":regno})
         result = self.cursor.fetchone()
         if result is None:
             #if nothing found, the regno is unique and does not exist yet
